66aa8682ed8777ea1dae710989eb8966aed01ae7
sunbaoqing <513857466@qq.com>
2019-01-22 18:39:46 +0800

    2019-1-22 18:39:38 创建项目

---
+++ code/app/src/main/java/com/chen/good/diary/LeadActivity.java
@@ -0,0 +1,13 @@
+package com.chen.good.diary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lead);
+    }
+}
